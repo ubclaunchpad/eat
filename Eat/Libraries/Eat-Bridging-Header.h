f490a14e36d2081d1c9c607fd5eca22fe6cd25cd
Milton Leung <milleung964@gmail.com>
2018-03-18 15:30:01 -0700

    Complete onboarding

---
+++ Eat/Libraries/Eat-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Eat-Bridging-Header.h
+//  Eat
+//
+//  Created by Milton Leung on 2018-03-18.
+//  Copyright © 2018 launchpad. All rights reserved.
+//
+
+#ifndef Eat_Bridging_Header_h
+#define Eat_Bridging_Header_h
+
+#import "OnboardingViewController.h"
+#import "OnboardingContentViewController.h"
+
+#endif /* Eat_Bridging_Header_h */
