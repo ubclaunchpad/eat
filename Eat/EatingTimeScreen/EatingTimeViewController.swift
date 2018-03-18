daa169dcbdaa519febad4ff91345e4b47f40f10a
Milton Leung <milleung964@gmail.com>
2018-03-17 18:35:36 -0700

    add gradient and pizza

--- Eat/EatingTimeScreen/EatingTimeViewController.swift
+++ Eat/EatingTimeScreen/EatingTimeViewController.swift
@@ -7,25 +7,34 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class EatingTimeViewController: UIViewController {
   @IBOutlet weak var headerLabel: UILabel!
   @IBOutlet weak var restaurantHoursLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
   @IBOutlet weak var restaurantHoursTextField: UITextField!
+  @IBOutlet weak var bottomGradientView: UIView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    applyStyling()
+    setDatePicker()
+    setGradient()
+  }
+
+  private func applyStyling() {
     headerLabel.font = Font.header(size: 13)
     headerLabel.textColor = #colorLiteral(red: 0.4196078431, green: 0.4352941176, blue: 0.6, alpha: 1)
     restaurantHoursLabel.font = Font.body(size: 22)
     restaurantHoursLabel.textColor = #colorLiteral(red: 0.3098039216, green: 0.3098039216, blue: 0.3098039216, alpha: 1)
     restaurantHoursTextField.font = Font.body(size: 22)
     restaurantHoursTextField.textColor = #colorLiteral(red: 0.2235294118, green: 0, blue: 0.8196078431, alpha: 1)
+  }
 
-    setDatePicker()
-    setDateLabel(date: "now")
-
+  private func setGradient() {
+    bottomGradientView.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: bottomGradientView.frame, andColors: [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0), #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)])
   }
 
   private func setDatePicker() {
@@ -65,11 +74,6 @@ class EatingTimeViewController: UIViewController {
 
   @objc func doneTapped() {
     restaurantHoursTextField.resignFirstResponder()
-  }
-
-  private func setDateLabel(date: String) {
-    dateLabel.attributedText = NSAttributedString(string: date, attributes:
-      [.underlineStyle: NSUnderlineStyle.styleSingle.rawValue])
   }
 
   private func setDateInput(date: String) {
